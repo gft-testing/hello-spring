312bd019fb7f7db10228e11d7722c5491b8fee8a
Javier Rodriguez <profesor@sinensia.com>
2022-01-25 09:23:24 +0100

    Added mapping / for root()

--- src/main/java/com/sinensia/demo/DemoProjectApplication.java
+++ src/main/java/com/sinensia/demo/DemoProjectApplication.java
@@ -14,6 +14,11 @@ public class DemoProjectApplication {
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String root() {
+		return "Hola ke ase";
+	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
